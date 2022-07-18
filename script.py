1075ae40ff277a45b8030f059ce2f5dfbefb0cbe
abatisse <abatisse@hevaweb.com>
2022-07-18 14:36:50 +0200

    fix: Remove unneeded .py file

--- script.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-with sqlite3.connect("data/movies.sqlite") as co:
-    print(
-        co.execute(
-            "select datetime(rating_timestamp, 'unixepoch') from ratings limit 10"
-        ).fetchall()
-    )
